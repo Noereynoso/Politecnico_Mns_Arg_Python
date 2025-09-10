d87ab3c162a88ec277175a8c624288cd41283f19
Tu Nombre <rnoe214500@gmail.com>
2025-09-10 18:54:28 -0300

    Subiendo lo mismo pero en nueva computadora

---
+++ Documents/Programación ll/Politecnico_Mns_Arg_Python-Primer-2ctm/Clase_2/Hoja1c2.py
@@ -0,0 +1,9 @@
+
+numeros = ["1", "2", "3", "4"]
+print(numeros [3])
+
+# Con esto accedemos al cuarto elemento de la lista, que es "4"
+
+# Tuplas que son?
+
+   
---
+++ Documents/Programación ll/Politecnico_Mns_Arg_Python-Primer-2ctm/Clase_2/eleplo.py
@@ -0,0 +1,2 @@
+diccionario = {"nombre": "Juan", "edad": 30, "carrera": "Datos"}
+print(diccionario) # Juan
